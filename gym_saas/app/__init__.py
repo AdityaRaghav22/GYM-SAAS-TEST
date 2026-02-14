52617b80f363906d1b9d690de464616620d87fec
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-14 05:18:19 +0000

    Db tables created

--- gym_saas/app/__init__.py
+++ gym_saas/app/__init__.py
@@ -82,8 +82,4 @@ def create_app():
     from gym_saas.app.routes.public import public_bp
     app.register_blueprint(public_bp)
     
-    with app.app_context():
-        db.create_all()
-
-
     return app
