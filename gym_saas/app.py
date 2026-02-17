40e6e8213a8b5ce00b96472cda340e941147387e
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-17 05:11:39 +0000

    Changes in list profile image size and optimizing performance

--- gym_saas/app.py
+++ gym_saas/app.py
@@ -3,4 +3,4 @@ from gym_saas.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=False)
