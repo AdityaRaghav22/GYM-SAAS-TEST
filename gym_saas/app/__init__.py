47e222aa56da2d6ad59c476c64bc7aa58207017a
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2026-02-23 15:02:12 +0000

    30 days login

--- gym_saas/app/__init__.py
+++ gym_saas/app/__init__.py
@@ -39,7 +39,7 @@ def create_app():
 
     # 🔥 expiry
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
-    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=1)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
     # 🔥 Render-safe cookie flags
     app.config["JWT_COOKIE_SECURE"] = True
